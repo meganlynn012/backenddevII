9c3ec6d251ccdcf9ec0078c7a0657e4e303663d8
Meg <megshaw@byui.edu>
2022-01-15 13:14:13 -0700

    week2 assignment2 done

--- week2-assignment2/app.js
+++ week2-assignment2/app.js
@@ -12,8 +12,24 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', (req, res, next) => {
-    res.render('add-product');
-})
+users = [];
+
+app.get('/', (req, res, next) => {
+    res.render('users', {
+        pageTitle: 'Users',
+        path: '/',
+        userList: users
+    });
+});
+app.post('/', (req, res, next) => {
+    users.push({ username: req.body.username});
+    res.redirect('/');
+    console.log(users);
+});
+/*app.get('/', (req, res, next) => {
+    res.render('users', {
+        userList: users
+    });
+});*/
 
 app.listen(2000);
