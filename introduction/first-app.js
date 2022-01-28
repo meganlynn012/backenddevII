0eb5250c6d783c45476a4f647bd4252ced9aabfe
Meg <megshaw@byui.edu>
2022-01-28 00:34:28 -0700

    updated folders to show current project

--- introduction/first-app.js
+++
@@ -1,6 +0,0 @@
-//type 'node first-app.js' in the terminal to run this code. Node runs independantly of the browser.
-//console.log("hello from Node.js");
-
-//typing 'node first-app.js' in the terminal will run this code and create a new file on my computer called 'hello.txt' with the 2nd argument as text in the file.
-const fs = require('fs');
-fs.writeFileSync('hello.txt', 'Hello from Node.js');
